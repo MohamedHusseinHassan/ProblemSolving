77a5e8ef7328e5431aeb613bd067d9d930a28969
mhussein <mhussein@mhussein-PC.Alfaris.com>
2019-10-21 09:54:46 +0200

    add another Solution

--- MasterDataStructuresAndAlgorithms/src/learn/dataStructure/hashTables/FirstRecurringNumber.java
+++ MasterDataStructuresAndAlgorithms/src/learn/dataStructure/hashTables/FirstRecurringNumber.java
@@ -7,19 +7,45 @@ public class FirstRecurringNumber {
 
 	public static void main(String[] args) {
 
-		int[] array = { 2, 5, 1, 2, 3, 5, 1, 2, 4 };
+		int[] array = { 2, 5, 5, 2, 3, 5, 1, 2, 4, 6 };
 
-		int firstRecurringNumber = findFirstRecurringNumber(array);
+		// int firstRecurringNumber = findFirstRecurringNumberFirstSolution(array);
+		int firstRecurringNumber = findFirstRecurringNumberSecondSolution(array);
 
 		System.out.println(firstRecurringNumber);
 
 	}
 
-	private static int findFirstRecurringNumber(int[] array) {
+	// O(n^2) Solution
+	private static int findFirstRecurringNumberFirstSolution(int[] array) {
+
+		int firstRecurringNumber = -1;
+
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+
+				if (array[i] == array[j]) {
+					firstRecurringNumber = array[i];
+					return firstRecurringNumber;
+				}
+
+				// special case that found two neighbour elements equal to each other
+				if (array[j] == array[j + 1] && (j < array.length - 1)) {
+					firstRecurringNumber = array[j];
+					return firstRecurringNumber;
+				}
+			}
+		}
+
+		return firstRecurringNumber;
+	}
+
+	// O(n) Solution
+	private static int findFirstRecurringNumberSecondSolution(int[] array) {
 
 		Map<Integer, Integer> recurringCount = new HashMap();
 
-		int firstRecurringNumber = 0;
+		int firstRecurringNumber = -1;
 
 		for (int i = 0; i < array.length; i++) {
 
