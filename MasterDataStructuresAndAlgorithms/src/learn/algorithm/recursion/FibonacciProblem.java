920630f5cd7e3d44e6450d2d187dc176916507f4
Moha <Moha@Moha-PC>
2019-10-26 15:49:59 +0200

    code refactor

--- MasterDataStructuresAndAlgorithms/src/learn/algorithm/recursion/FibonacciProblem.java
+++ MasterDataStructuresAndAlgorithms/src/learn/algorithm/recursion/FibonacciProblem.java
@@ -3,28 +3,30 @@ package learn.algorithm.recursion;
 public class FibonacciProblem {
 
 	public static void main(String[] args) {
-		
-		
-		// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+		// Given a number N return the index value of the Fibonacci sequence,
+		// where the sequence is:
 
 		// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
-		// the pattern of the sequence is that each value is the sum of the 2 previous values, 
+		// the pattern of the sequence is that each value is the sum of the 2
+		// previous values,
 		// that means that for N=5 → 2+3
 
-		//For example: fibonacciRecursive(6) should return 8
-		
+		// For example: fibonacciRecursive(6) should return 8
 
-		
-		
-//		int output = fibonacciIterative(6);
-		
+		// int output = fibonacciIterative(6);
+
 		int output = fibonacciRecursive(7);
-		
+
 		System.out.println(output);
 
 	}
 
 	static int fibonacciIterative(int input) {
+
+		if (input == 0) {
+			return 0;
+		}
 
 		if (input == 1 || input == 2) {
 			return 1;
@@ -41,14 +43,13 @@ public class FibonacciProblem {
 
 		return output[output.length - 1];
 	}
-	
 
-	 static int fibonacciRecursive(int input) {
-	     if(input == 1 || input == 2){
-	    	 return 1;
-	     }
-	
-		 return fibonacciRecursive(input-1) + fibonacciRecursive(input-2);
-	 }
+	static int fibonacciRecursive(int input) {
+		if (input < 2) {
+			return input;
+		}
+
+		return fibonacciRecursive(input - 1) + fibonacciRecursive(input - 2);
+	}
 
 }
