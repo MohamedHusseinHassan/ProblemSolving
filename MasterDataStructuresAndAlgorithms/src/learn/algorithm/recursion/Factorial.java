bb0349f26932fcf08cf8aa719c354cc3e0b151e2
Moha <Moha@Moha-PC>
2019-10-26 13:30:17 +0200

    implementation of factorial

---
+++ MasterDataStructuresAndAlgorithms/src/learn/algorithm/recursion/Factorial.java
@@ -0,0 +1,36 @@
+package learn.algorithm.recursion;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+//		int output = findFactorialIterative(5);
+//		System.out.println(output);
+		
+		int output = findFactorialRecursive(5);
+		System.out.println(output);
+		
+	}
+
+	static int findFactorialIterative(int input) {
+
+		int output = 1;
+
+		for (int i = input; i > 0; i--) {
+
+			output = output * i;
+		}
+
+		return output;
+	}
+
+	static int findFactorialRecursive(int input) {
+
+		if (input == 1) {
+			return 1;
+		}
+
+		return input * findFactorialRecursive(input - 1);
+	}
+
+}
